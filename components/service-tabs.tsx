fc59e5d0f872531c8001e4d086c2d5dfcd0d5d2f
KunalRai7 <oneplus.kunal.rai@gmail.com>
2024-09-27 13:59:16 +0530

    icon

--- components/service-tabs.tsx
+++ components/service-tabs.tsx
@@ -5,6 +5,7 @@ import { Tabs, TabsList, TabsTrigger, TabsContent } from "@/components/ui/tabs"
 import InteriorTab from './interior-tab'
 import ExteriorTab from './exterior-tab'
 import BathroomTab from './bathroom-tab'
+import { Home, Building, Bath } from 'lucide-react'  // Import the icons
 
 export default function ServiceTabs() {
   const [activeTab, setActiveTab] = useState('interior')
@@ -12,9 +13,15 @@ export default function ServiceTabs() {
   return (
     <Tabs value={activeTab} onValueChange={setActiveTab} className="w-full max-w-3xl mx-auto">
       <TabsList className="grid w-full grid-cols-3 mb-2">
-        <TabsTrigger value="interior">Interior</TabsTrigger>
-        <TabsTrigger value="exterior">Exterior</TabsTrigger>
-        <TabsTrigger value="bathroom">Bathroom</TabsTrigger>
+        <TabsTrigger value="interior">
+          <Home className="w-4 h-4 mr-2" /> Interior
+        </TabsTrigger>
+        <TabsTrigger value="exterior">
+          <Building className="w-4 h-4 mr-2" /> Exterior
+        </TabsTrigger>
+        <TabsTrigger value="bathroom">
+          <Bath className="w-4 h-4 mr-2" /> Bathroom
+        </TabsTrigger>
       </TabsList>
       <TabsContent value="interior">
         <InteriorTab />
